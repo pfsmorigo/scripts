bbdb905fa55afcbae48943ca7fe335f66c8fdabd
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2017-07-30 13:04:56 -0300

    7dtd: time left working

--- games/7dtd.py
+++ games/7dtd.py
@@ -27,15 +27,14 @@ servertime = int(''.join(servertime_line).split(":")[1])
 players_line = [s for s in dump if "CurrentPlayers" in s]
 players = int(''.join(players_line).split(":")[1])
 
-day = int((servertime/24000)+1)
+day = (servertime/24000)+1
 week = day%7
+store = day%3
 hour = int(float((servertime%24000)/1000))
 mins = int(float(float(servertime%1000)*60)/1000)
 
-time_next = 4 if (hour < 4 or hour > 22) else 22
-left_secs = time.time()+((time_next-hour)*60)+(60-mins)*2.5
-left_time = time.strptime(time.ctime(left_secs))
-left = time.strftime("%Hh%M", left_time)
+time_next = 4 if (hour < 4 or hour >= 22) else 22
+time_next += 24 if (time_next < hour) else 0
+left = (((time_next-hour)*60-(60-mins))/60.)*2.5
 
-#print "%d/%d %dh%02d %s %d %d" % (day, week, hour, mins, left, players)
-print "%d/%d %dh%02d %d" % (day, week, hour, mins, players)
+print "%d/%d/%d %dh%02d(%d) %d" % (day, week, store, hour, mins, left, players)
