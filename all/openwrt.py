0f58d992bc993ffb17cc40b1686c41e751dd58e4
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2024-08-17 09:36:37 -0300

    openwrt.py: Add ping, get devs automatically and bug fixes
    Signed-off-by: Paulo Flabiano Smorigo <pfsmorigo@gmail.com>

--- all/openwrt.py
+++ all/openwrt.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
+import argparse
+import re
+import sys
 import subprocess
-
-ROUTERS = ["brno", "extender"]
-# ROUTERS = ["router"]
-WLANS = ["wlan0", "wlan1"]
 
 PRI_5G = 94
 PRI_2G = 34
 SEC_5G = 92
 SEC_2G = 32
 MOVING = 93
+ONLINE = 35
+OFFLINE = 37
 
 def get_dhcp_lease(host):
     result = {}
@@ -25,11 +26,18 @@ def get_dhcp_lease(host):
             result[mac] = {"ipv4": ipv4, "hostname": hostname}
     return result
 
-def get_iw_info(host, wlan):
+def get_iw_devs(host):
+    result = []
+    command = subprocess.run(["ssh", host, "iw", "dev"], stdout=subprocess.PIPE)
+    string = command.stdout.decode("utf-8")
+    for line in [l for l in string.split("\n") if 'Interface' in l]:
+        result.append(line.split()[1])
+    return result
+
+def get_iw_info(host, dev):
     result = {}
     station = ""
-
-    command = subprocess.run(["ssh", host, "iw", "dev", wlan, "station", "dump"], stdout=subprocess.PIPE)
+    command = subprocess.run(["ssh", host, "iw", "dev", dev, "station", "dump"], stdout=subprocess.PIPE)
     string = command.stdout.decode("utf-8")
     for line in string.split("\n"):
         if line.startswith("Station"):
@@ -41,23 +49,42 @@ def get_iw_info(host, wlan):
                 result[station][field] = line.split(":")[1].strip()
     return result
 
-dhcp = get_dhcp_lease("brno")
+def load_iw_info(routers):
+    result = {}
+    for router in routers:
+        result[router] = {}
+        for dev in get_iw_devs(router):
+            result[router][dev] = get_iw_info(router, dev)
+    return result
 
-iw_info = {}
-for router in ROUTERS:
-    iw_info[router] = {}
-    for wlan in WLANS:
-        iw_info[router][wlan] = get_iw_info(router, wlan)
+def run_ping(ip, timeout=3):
+    status, result = subprocess.getstatusoutput(f'ping -c1 -w{timeout} {ip}')
+    matches = re.findall(r"\btime=(\d+(?:\.\d+)?)\s+ms\b", result)
+    try:
+        return round(float(matches[0]), 1) if status == 0 else ''
+    except:
+        return '?'
+
+parser = argparse.ArgumentParser(description="OpenWRT status")
+parser.add_argument("-p", "--ping", action="store_true", help="Ping all hosts")
+parser.add_argument("--on", action="store_true", help="Show only online hosts")
+parser.add_argument("--off", action="store_true", help="Show only offline hosts")
+parser.add_argument("devices", nargs='+', metavar='DEVICE', help="Device list")
+
+args = parser.parse_args()
+
+dhcp = get_dhcp_lease(args.devices[0])
+iw_info = load_iw_info(args.devices)
 
 for mac in sorted(dhcp, key=lambda x: tuple(int(p) for p in dhcp[x]["ipv4"].split('.'))):
     routers = []
     wlans = []
 
-    for router in ROUTERS:
-        for wlan in WLANS:
-            if mac in iw_info[router][wlan]:
+    for router in args.devices:
+        for dev in iw_info[router]:
+            if mac in iw_info[router][dev]:
                 routers.append(router)
-                wlans.append(wlan)
+                wlans.append(dev)
 
     hostname = dhcp[mac]["hostname"]
     ipv4 = dhcp[mac]["ipv4"]
@@ -66,9 +93,11 @@ for mac in sorted(dhcp, key=lambda x: tuple(int(p) for p in dhcp[x]["ipv4"].spli
     rx = ""
     tx = ""
     signal = ""
+    ping = ""
 
     if not routers:
-        color = 37
+        ping = run_ping(ipv4)
+        color = ONLINE if ping else OFFLINE
     else:
         rx = " ".join(iw_info[routers[0]][wlans[0]][mac]["rx bitrate"].split(" ")[:2])[:-6]
         tx = " ".join(iw_info[routers[0]][wlans[0]][mac]["tx bitrate"].split(" ")[:2])[:-6]
@@ -78,11 +107,16 @@ for mac in sorted(dhcp, key=lambda x: tuple(int(p) for p in dhcp[x]["ipv4"].spli
 
         if "both" in [router, wlan]:
             color = MOVING
-        elif "extender" not in routers:
+        elif args.devices[0] in routers:
             color = PRI_5G if "wlan1" not in wlans else PRI_2G
         else:
             color = SEC_5G if "wlan1" not in wlans else SEC_2G
 
-    print("\033[%dm%-18s %-25s %-11s %-9s %-5s %7s %7s %-24s\033[0m"
-            % (color, mac, hostname, ipv4, router, wlan, rx, tx, signal))
+    if ping != '' and not args.ping:
+        ping = ''
+    elif ping == '' and args.ping:
+        ping = run_ping(ipv4)
+
+    print("\033[%dm%-18s %-25s %-11s %5s  %-9s %-5s %7s %7s %-24s\033[0m"
+            % (color, mac, hostname, ipv4, ping, router, wlan, rx, tx, signal))
 
